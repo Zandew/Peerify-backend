444913a6c11da6ebc5a24d1f7994c07cd073c2ef
William Li <liwilgq@gmail.com>
2020-08-22 13:34:46 -0400

    updated model information

--- models/Room.js
+++ models/Room.js
@@ -1,10 +1,26 @@
 const mongoose = require('mongoose');
 
+class UserFeedback {
+    constructor(text, rating) {
+        this.text = text;
+        this.rating = rating;
+    }
+}
+
 const RoomSchema = mongoose.Schema({
     _id: String,
     users: [String],
     leader: String,
-    submissions: Number
+    users_ready: Number,
+
+    total_rounds: Number,
+    rounds_done: Number,
+
+    prompt: String,
+    user_entries: [String],
+    user_feedback: [UserFeedback],
+
+    scores: [Number],
 });
 
 const Room = mongoose.model('Room', RoomSchema);
